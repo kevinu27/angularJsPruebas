aaffaaa3bdb7d6bcfb96c3ec90308e486c3add6a
SANTANA MEDINA <O006188@ad.bbva.com>
2023-06-30 14:22:53 +0200

    video 10 - seteando el entorno de trabajo

---
+++ js/app.js
@@ -0,0 +1,13 @@
+var app = angular.module('yugiohApp', []) // yugioh es el nombre del proyecto
+
+app.controller('yugiohCardsCrtl', function(){
+
+    this.cards = cards
+
+
+})
+
+var cards = {
+    carta1: 'mago',
+    carta2: 'dragon'
+}
