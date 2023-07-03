f79fe8545b2b9d60ce1c6a3b4a42df02623d6867
SANTANA MEDINA <O006188@ad.bbva.com>
2023-07-03 15:12:54 +0200

    se ven todas las cartas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 var app = angular.module('yugiohApp', []) // yugioh es el nombre del proyecto
 
-app.controller('yugiohCardsCrtl', function($scope){
+app.controller('yugiohCardsCrtl', ['$scope', '$http', function($scope, $http){
 
     this.cards = cards
     $scope.cards = cards
@@ -11,9 +11,23 @@ app.controller('yugiohCardsCrtl', function($scope){
         $scope.mostrarBoton2 = !$scope.mostrarBoton2
     }
     $scope.listaParaBucle = ['a', 'v', 'c']
+    $scope.jsonCards = {}
+
+    $http.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').then(function(data){
+        $scope.jsonCards = data
+
+        console.log($scope.jsonCards.data.data[100].card_images[0].image_url)
+        console.log($scope.jsonCards.data.data)
+        $scope.jsonCardsArrayToIterate = [] 
+        $scope.jsonCards.data.data.forEach(element => {
+            $scope.jsonCardsArrayToIterate.push(element.card_images[0].image_url)
+        });
+        console.log('$scope.jsonCardsArrayToIterate', $scope.jsonCardsArrayToIterate)
+
+    })
 
 
-})
+}])
 
 const cards = {
     carta1: 'mago',
