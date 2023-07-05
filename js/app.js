8299eee6d38f803f0ae60f5ee465407dc544f984
kevin.santana@bbvaitspain.com <kevin.santana@bbvaitspain.com>
2023-07-05 08:45:02 +0200

    se ven las cartas como en grid

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ app.controller('yugiohCardsCrtl', ['$scope', '$http', function($scope, $http){
     this.cards = cards
     $scope.cards = cards
     $scope.mostrarBoton2 = false
+    $scope.rutaHtml ='parciales/navbar.html'
 
     $scope.mostrarNombreCard = function(){
         console.log('$scope.cards')
@@ -12,20 +13,35 @@ app.controller('yugiohCardsCrtl', ['$scope', '$http', function($scope, $http){
     }
     $scope.listaParaBucle = ['a', 'v', 'c']
     $scope.jsonCards = {}
+    $scope.jsonCards2 = {}
 
     $http.get('https://db.ygoprodeck.com/api/v7/cardinfo.php').then(function(data){
-        $scope.jsonCards = data
+        // $scope.jsonCards = data
 
-        console.log($scope.jsonCards.data.data[100])
-        console.log($scope.jsonCards.data.data)
+        // console.log($scope.jsonCards.data.data[100])
+        // console.log($scope.jsonCards.data.data)
+        // $scope.jsonCardsArrayToIterate = [] 
+        // $scope.jsonCards.data.data.forEach(element => {
+        //     $scope.jsonCardsArrayToIterate.push(element)
+        // });
+        // console.log('$scope.jsonCardsArrayToIterate', $scope.jsonCardsArrayToIterate)
+
+    })
+
+    $http.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?type=Normal Monster').then(function(data2){
+        $scope.jsonCards2 = data2
+
+        console.log($scope.jsonCards2)
+        console.log($scope.jsonCards2.data.data[100])
+        console.log($scope.jsonCards2.data.data)
         $scope.jsonCardsArrayToIterate = [] 
-        $scope.jsonCards.data.data.forEach(element => {
+        $scope.jsonCards2.data.data.forEach(element => {
             $scope.jsonCardsArrayToIterate.push(element)
         });
         console.log('$scope.jsonCardsArrayToIterate', $scope.jsonCardsArrayToIterate)
 
-    })
 
+    })
 
 }])
 
